71ef5ac596f45285551dbb26d8d8c399069db839
qiaoyansong <1012077930@qq.com>
2021-05-26 12:09:05 +0800

    update

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,8 @@ package main;
 import util.ActiveMqUtil;
 
 import javax.jms.*;
-import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author ：Qiao Yansong
@@ -12,58 +13,93 @@ import java.io.IOException;
  */
 public class Main {
 
-    private static final String QUEUE_NAME = "queue1";
+    private static final String TOPIC_NAME = "topic1";
 
     public static void main(String[] args) {
-        testConsumerUseReceive();
+        testParallel();
+    }
+
+    /**
+     * 两个消费者查看消费的信息
+     */
+    private static void testParallel() {
+        ExecutorService THREAD_POOl = Executors.newFixedThreadPool(2);
+        try {
+            for (int i = 0; i < 2; i++) {
+                THREAD_POOl.execute(() -> {
+                    System.out.println("这是消费者" + Thread.currentThread().getName());
+                    XAConnection connection = null;
+                    Session session = null;
+                    MessageConsumer consumer = null;
+                    try {
+                        connection = ActiveMqUtil.getConnection();
+                        connection.start();
+                        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                        Topic topic = session.createTopic(TOPIC_NAME);
+                        consumer = session.createConsumer(topic);
+                        while (true) {
+                            TextMessage message = (TextMessage) consumer.receive();
+                            System.out.println("消费者" + Thread.currentThread().getName() + "消费的消息是" + message.getText());
+                        }
+
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (consumer != null) {
+                            try {
+                                connection.close();
+                            } catch (JMSException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (session != null) {
+                            try {
+                                session.close();
+                            } catch (JMSException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (connection != null) {
+                            try {
+                                connection.close();
+                            } catch (JMSException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+        } finally {
+            if (THREAD_POOl != null) {
+                THREAD_POOl.shutdown();
+            }
+        }
     }
 
     /**
      * 消费者使用receive接受消息
      */
-    private static void testConsumerUseReceive(){
-
-    }
-
-    /**
-     * 消费者使用listener接受消息
-     */
-    private static void testConsumerUseListener(){
+    private static void testConsumerUseReceive() {
         XAConnection connection = null;
         Session session = null;
         MessageConsumer consumer = null;
         try {
             connection = ActiveMqUtil.getConnection();
             connection.start();
-            // 创建Session，有两个参数，分别是是否开启事务、签收
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            // 创建目的地（可以是队列也可以是Topic）
-            Queue queue = session.createQueue(QUEUE_NAME);
-            // 创建消息消费者
-            consumer = session.createConsumer(queue);
-            consumer.setMessageListener(new MessageListener() {
-                @Override
-                public void onMessage(Message message) {
-                    if(message != null && message instanceof TextMessage){
-                        TextMessage textMessage = (TextMessage) message;
-                        try {
-                            System.out.println("消费者收到的消息是" + textMessage.getText());
-                        } catch (JMSException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-            // 必要的步骤，防止消费者还没连接到mq，就被关闭了
-            System.in.read();
+            Topic topic = session.createTopic(TOPIC_NAME);
+            consumer = session.createConsumer(topic);
+            while (true) {
+                TextMessage message = (TextMessage) consumer.receive();
+                System.out.println("消费的消息是" + message.getText());
+            }
+
         } catch (JMSException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             if (consumer != null) {
                 try {
-                    consumer.close();
+                    connection.close();
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
@@ -84,4 +120,5 @@ public class Main {
             }
         }
     }
+
 }
